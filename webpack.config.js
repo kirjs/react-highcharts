70a26f73018355930344a2dc674fa13e131a7e84
aldendaniels <alden@aldendaniels.me>
2015-08-18 10:17:27 -0500

    Support mixing use of index.js and more.js
    Resolves #9

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var path = require('path');
+
 module.exports = {
   entry: {
     highcharts: ['./src/Highcharts.jsx'], // Array syntax to workaround https://github.com/webpack/webpack/issues/300
@@ -11,10 +13,22 @@ module.exports = {
       }
     ]
   },
-  externals: {
-    'react/addons': 'react/addons',
-    'react': 'react'
-  },
+  externals:
+  [
+    {
+      'react/addons': true,
+      'react': true
+    },
+    function(context, request, callback) {
+      if (request === './Highcharts.jsx') {
+        // Exclude highcharts.js from more.js
+        // Resolves https://github.com/kirjs/react-highcharts/issues/9
+        callback(null, './highcharts');
+      } else {
+        callback();
+      }
+    }
+  ],
   resolve: {
     alias: {
       "highcharts" : "highcharts-release/highcharts.src.js",
