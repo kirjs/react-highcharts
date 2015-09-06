d9988caf71faf2b2277b1449202c75ba615ce0c5
Kirill Cherkashin <kirill@kcherkashin.org>
2015-09-05 21:00:32 -0400

    Add simulateDOM function, which simulates just enough DOM to keep Highcharts happy and quiet when rendering on node.
    This is a prerequisite for adding tests

---
+++ src/simulateDOM.js
@@ -0,0 +1,24 @@
+/**
+ * This function simulates just enough DOM to keep Highcharts happy and quiet when rendering on node.
+ * It can be used for testing, and also when using reach-highcharts in isomorphic apps (but very carefully)
+ *
+ * It is based on JSDOM, which means 2 things:
+ *
+ * 1. You have to use it outside of webpack (see https://github.com/kirjs/react-highcharts/issues/12)
+ * 2. Highcharts won't actually render anything as JSDOM as of now doesn't really support SVG Rendering
+ * (there are multiple issues spread around the project, e.g. https://github.com/tmpvar/jsdom/issues/620)
+ *
+ * In order to use it just include this file before requiring Highcharts.
+ */
+
+var jsdom = require('jsdom');
+
+global.document = jsdom.jsdom('<!doctype html><html><body><div id="test"></div></body></html>');
+var win = global.document.defaultView;
+global.window = global;
+for( var i in win ){
+  if( i !== 'window' && win.hasOwnProperty(i)){
+    global.window[i] = win[i];
+  }
+}
+
