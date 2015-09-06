b8e31a26b741f94a13a798ffcc1f1b60e7764676
Kirill Cherkashin <kirill@kcherkashin.org>
2015-09-05 21:23:29 -0400

    Mention in the comment, that simulateDOM only works with IO.js

--- src/simulateDOM.js
+++ src/simulateDOM.js
@@ -2,11 +2,12 @@
  * This function simulates just enough DOM to keep Highcharts happy and quiet when rendering on node.
  * It can be used for testing, and also when using reach-highcharts in isomorphic apps (but very carefully)
  *
- * It is based on JSDOM, which means 2 things:
+ * It is based on JSDOM, which means 3 things:
  *
  * 1. You have to use it outside of webpack (see https://github.com/kirjs/react-highcharts/issues/12)
  * 2. Highcharts won't actually render anything as JSDOM as of now doesn't really support SVG Rendering
  * (there are multiple issues spread around the project, e.g. https://github.com/tmpvar/jsdom/issues/620)
+ * 3. It will only run on IO.js, not on node.
  *
  * In order to use it just include this file before requiring Highcharts.
  */
