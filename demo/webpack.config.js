1f349bfd8917f6d7f4189e976c333c3f17edf0f0
Mats Oustad <oustad@gmail.com>
2015-09-17 09:34:16 +0200

    Removed wrong formatting and wrong path in npm run script

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,33 +1,33 @@
 module.exports = {
-    entry: {
-        index: './src/index.jsx',
-        more: './src/more.jsx',
-        highstock: './src/highstock.jsx',
-        exportable: './src/exportable.jsx'
+  entry: {
+    index: './src/index.jsx',
+    more: './src/more.jsx',
+    highstock: './src/highstock.jsx',
+    exportable: './src/exportable.jsx'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx$/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    alias: {
+      "highstock": "highstock-release/highstock.src.js",
+      "highcharts": "highcharts-release/highcharts.src.js",
+      "highcharts-more": "highcharts-release/highcharts-more.src.js",
+      "highcharts-standalone-adapter": "highcharts-release/adapters/standalone-framework.src.js",
+      "exporting": "highcharts-release/modules/exporting.src.js",
+      "react-highcharts/more": '../../src/More.jsx',
+      "react-highcharts/highstock": '../../src/Highstock.jsx',
+      "react-highcharts/exportable": '../../src/ExportableHighcharts.jsx',
+      "react-highcharts": '../../src/Highcharts.jsx'
     },
-    module: {
-        loaders: [{
-            test: /\.jsx$/,
-            loader: 'babel'
-        }]
-    },
-    resolve: {
-        alias: {
-            "highstock": "highstock-release/highstock.src.js",
-            "highcharts": "highcharts-release/highcharts.src.js",
-            "highcharts-more": "highcharts-release/highcharts-more.src.js",
-            "highcharts-standalone-adapter": "highcharts-release/adapters/standalone-framework.src.js",
-            "exporting": "highcharts-release/modules/exporting.src.js",
-            "react-highcharts/more": '../../src/More.jsx',
-            "react-highcharts/highstock": '../../src/Highstock.jsx',
-            "react-highcharts/exportable": '../../src/ExportableHighcharts.jsx',
-            "react-highcharts": '../../src/Highcharts.jsx'
-        },
-        modulesDirectories: ['node_modules']
-    },
-    output: {
-        path: 'dist/',
-        filename: '[name].js',
-        publicPath: '/'
-    }
+    modulesDirectories: ['node_modules']
+  },
+  output: {
+    path: 'dist/',
+    filename: '[name].js',
+    publicPath: '/'
+  }
 };
