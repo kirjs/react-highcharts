73046d60b115dd64b96326a0ac4615a34cd2901d
Kirill Cherkashin <kirill@kcherkashin.org>
2015-11-17 10:27:01 -0500

    Actually copy the bundles to the top level

--- src/copy-bundles.sh
+++ src/copy-bundles.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+cd "$(git rev-parse --show-toplevel)"
 rm -rf ./bundle
-cp -r ./dist/bundle ./
+cp -r ./dist/bundle/highcharts.js ./
+cp -r ./dist/bundle/highstock.js ./
+cp -r ./dist/bundle/highmaps.js ./
