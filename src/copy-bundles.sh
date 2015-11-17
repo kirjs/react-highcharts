03b8cb6b001aa23e5de79ac725c0d27f2e13ab22
Kirill Cherkashin <kirill@kcherkashin.org>
2015-11-17 10:35:17 -0500

    roll back 5.0.3, as  it wasn't a good idea

--- src/copy-bundles.sh
+++ src/copy-bundles.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-cd "$(git rev-parse --show-toplevel)"
 rm -rf ./bundle
-cp -r ./dist/bundle/highcharts.js ./
-cp -r ./dist/bundle/highstock.js ./
-cp -r ./dist/bundle/highmaps.js ./
+cp -r ./dist/bundle ./
