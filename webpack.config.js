32159008b0e679d5e171090d530af9f521e13c2d
Kirill Cherhkashin <kirill@kcherkashin.org>
2017-04-27 00:47:20 -0400

    Fix webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,95 +1,101 @@
 const path = require('path');
 
-/**
- * If -p flag is set, minify the files
- * @type {boolean}
- */
-const src = (process.argv.indexOf('-p') === -1);
-const filenamePostfix = src ? '.src' : '';
 
-/**
- * If -b flag is set, build bundles, and not exclude highcharts from the build
- * @type {boolean}
- */
-const bundles = (process.argv.indexOf('-b') !== -1);
-const bundlePrefix = (bundles ? 'bundle/' : '');
+module.exports = function (env) {
+    env = env || {};
 
 
-const highchartsExternals = {
-    'highcharts/highmaps': {
-        root: 'Highcharts',
-        commonjs2: 'highcharts/highmaps',
-        commonjs: 'highcharts/highmaps',
-        amd: 'highcharts/highmaps'
-    },
-    'highcharts/highstock': {
-        root: 'Highcharts',
-        commonjs2: 'highcharts/highstock',
-        commonjs: 'highcharts/highstock',
-        amd: 'highcharts/highstock'
-    },
-    'highcharts': {
-        root: 'Highcharts',
-        commonjs2: 'highcharts',
-        commonjs: 'highcharts',
-        amd: 'highcharts'
-    }
-};
+    /**
+     * If -p flag is set, minify the files
+     * @type {boolean}
+     */
+    const src = !env.p;
+    const filenamePostfix = src ? '.src' : '';
 
-const reactExternals = {
-    react: {
-        root: 'React',
-        commonjs2: 'react',
-        commonjs: 'react',
-        amd: 'react'
-    },
-    'react-dom': {
-        root: 'ReactDOM',
-        commonjs2: 'react-dom',
-        commonjs: 'react-dom',
-        amd: 'react-dom'
-    }
-};
-
-const externals = [reactExternals];
-
-externals.push(highchartsExternals);
+    /**
+     * If -b flag is set, build bundles, and not exclude highcharts from the build
+     * @type {boolean}
+     */
+    const bundles = env.b;
+    const bundlePrefix = (bundles ? 'bundle/' : '');
 
 
-module.exports = {
-    entry: {
-        // Array syntax to workaround https://github.com/webpack/webpack/issues/300
-        'index': ['./src/ReactHighcharts.jsx'],
-        'ReactHighcharts': ['./src/ReactHighcharts.jsx'],
-        'ReactHighstock': ['./src/Highstock.jsx'],
-        'ReactHighmaps': ['./src/Highmaps.jsx'],
-        'RedrawOnPrint': ['./src/RedrawOnPrint.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx$/,
-                use: [{
-                    loader: 'babel-loader',
-                    query: {
-                        cacheDirectory: true,
-                        presets: ['react', 'es2015', 'stage-2']
-                    }
-                }],
+    const highchartsExternals = {
+        'highcharts/highmaps': {
+            root: 'Highcharts',
+            commonjs2: 'highcharts/highmaps',
+            commonjs: 'highcharts/highmaps',
+            amd: 'highcharts/highmaps'
+        },
+        'highcharts/highstock': {
+            root: 'Highcharts',
+            commonjs2: 'highcharts/highstock',
+            commonjs: 'highcharts/highstock',
+            amd: 'highcharts/highstock'
+        },
+        'highcharts': {
+            root: 'Highcharts',
+            commonjs2: 'highcharts',
+            commonjs: 'highcharts',
+            amd: 'highcharts'
+        }
+    };
+
+    const reactExternals = {
+        react: {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react'
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom'
+        }
+    };
+
+    const externals = [reactExternals];
+
+    externals.push(highchartsExternals);
 
 
-            }
-        ]
-    },
-    externals: externals,
-    resolve: {
-        modules: [
-            "node_modules"
-        ]
-    },
-    output: {
-        filename: 'dist/' + bundlePrefix + '[name]' + filenamePostfix + '.js',
-        libraryTarget: 'umd',
-        library: '[name]'
+    return {
+        entry: {
+            // Array syntax to workaround https://github.com/webpack/webpack/issues/300
+            'index': ['./src/ReactHighcharts.jsx'],
+            'ReactHighcharts': ['./src/ReactHighcharts.jsx'],
+            'ReactHighstock': ['./src/Highstock.jsx'],
+            'ReactHighmaps': ['./src/Highmaps.jsx'],
+            'RedrawOnPrint': ['./src/RedrawOnPrint.jsx']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx$/,
+                    use: [{
+                        loader: 'babel-loader',
+                        query: {
+                            cacheDirectory: true,
+                            presets: ['react', 'es2015', 'stage-2']
+                        }
+                    }],
+
+
+                }
+            ]
+        },
+        externals: externals,
+        resolve: {
+            modules: [
+                "node_modules"
+            ]
+        },
+        output: {
+            filename: 'dist/' + bundlePrefix + '[name]' + filenamePostfix + '.js',
+            libraryTarget: 'umd',
+            library: '[name]'
+        }
     }
 };
