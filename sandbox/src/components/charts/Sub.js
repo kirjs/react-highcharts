1c7875fdf507fd8edc39f18c0e3fed5761b9c444
voicetime <iprovod@gmail.com>
2018-01-28 03:36:28 +0300

    newsrt -> result

--- sandbox/src/components/charts/Sub.js
+++ sandbox/src/components/charts/Sub.js
@@ -7,9 +7,9 @@ class Sub extends Component {
 
     let re = new RegExp("([\\t ]*" + "{(.*?)" + "\\/\\* ?" + 'strip-start' + ")[\\s\\S]*?(" + 'strip-end' + " ?\\*\\/" + "(.*?)}" + "[\\t ]*\\r?\\n?)", "g");
     let re2 = new RegExp("([\\t ]*\\/\\* ?" + 'strip-start' + ")[\\s\\S]*?(" + 'strip-end' + " ?\\*\\/[\\t ]*\\r?\\n?)", "g");
-    let newstr = children[1].replace(re, "");
-    newstr = newstr.replace(re2, "");
-    return newstr;
+    let result = children[1].replace(re, "");
+    result = result.replace(re2, "");
+    return result;
   }
 
   render() {
