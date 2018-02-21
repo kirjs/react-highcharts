62185f3683d117c7061d3cd1e80131be3bcb31f9
voicetime <iprovod@gmail.com>
2018-02-21 21:51:56 +0300

    del ducks/charts.js in reducer

--- sandbox/src/reducer/index.js
+++ sandbox/src/reducer/index.js
@@ -1,8 +1,6 @@
 import {combineReducers} from 'redux'
 import {routerReducer} from 'react-router-redux'
-import charts from './ducks/charts';
 
 export default combineReducers({
   router: routerReducer,
-  charts
 })
