6d23fdf7a2fd5c0f6d1ea462ce609a1ea9c01bcc
voicetime <iprovod@gmail.com>
2018-03-11 18:00:42 +0300

    add prettier

--- sandbox/src/components/charts/CodeWrapper.js
+++ sandbox/src/components/charts/CodeWrapper.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 import sdk from "@stackblitz/sdk";
 
 class CodeWrapper extends Component {
-  componentDidMount(){
-    const{name, files} = this.props;
+  componentDidMount() {
+    const {name, files} = this.props;
     files['index.js'] = `import React, { Component } from 'react';
 import { render } from 'react-dom';
 import ${name} from './${name}';
@@ -25,16 +25,17 @@ render(<App />, document.getElementById('root'));
       title: 'React-Highcharts',
       description: 'React-Highcharts',
       template: 'create-react-app',
-      tags: ['stackblitz', 'sdk', 'React-Highcharts'] ,
+      tags: ['stackblitz', 'sdk', 'React-Highcharts'],
       dependencies: {
         'react': '16.2.0',
-        'react-dom':'16.2.0',
-        'react-highcharts':'16.0.2',
-         'highcharts': '6.0.7'
+        'react-dom': '16.2.0',
+        'react-highcharts': '16.0.2',
+        'highcharts': '6.0.7'
       },
     };
-    sdk.embedProject(this.stackblitz, project, { height: 550, width:'100%', forceEmbedLayout:true,view:'preview' });
+    sdk.embedProject(this.stackblitz, project, {height: 550, width: '100%', forceEmbedLayout: true, view: 'preview'});
   }
+
 
   componentWillUnmount() {
     this.stackblitz = null;
@@ -42,16 +43,15 @@ render(<App />, document.getElementById('root'));
 
   render() {
     let {children} = this.props;
-    return (
-      <div>
-        <div ref={sb => this.stackblitz = sb} > </div>
-        <PrismCode component="pre" className="language-javascript">
-          {children}
-        </PrismCode>
-      </div>
-    );
+    return <div>
+      <div ref={sb => this.stackblitz = sb}></div>
+      <PrismCode component="pre" className="language-javascript">
+        {children}
+      </PrismCode>
+    </div>;
   }
 }
+
 
 CodeWrapper.propTypes = {
   files: PropTypes.object.isRequired,
