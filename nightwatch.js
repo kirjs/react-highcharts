afe198681abb970973b89ad43d13f023bfc28479
voicetime <iprovod@gmail.com>
2018-08-27 00:02:36 +0300

    key hard code

--- nightwatch.js
+++ nightwatch.js
@@ -13,7 +13,7 @@ module.exports = {
         build: 'build-' + TRAVIS_JOB_NUMBER,
         'tunnel-identifier': TRAVIS_JOB_NUMBER,
         "username": process.env.SAUCE_USERNAME,
-        "access_key": process.env.SAUCE_ACCESS_KEY
+        "access_key": "2addb26e-5725-40c8-b1da-8778cb845253"
       },
       "launch_url": "http://127.0.0.1:8080/",
       "selenium_port": 80,
